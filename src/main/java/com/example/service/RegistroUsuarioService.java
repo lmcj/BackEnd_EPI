3874920a192b3e1f9dee6c235ceef219f6dbf814
lmcj <lmcj>
2024-07-23 23:48:08 -0500

    Add EncriptarContraseña

--- src/main/java/com/example/service/RegistroUsuarioService.java
+++ src/main/java/com/example/service/RegistroUsuarioService.java
@@ -10,14 +10,19 @@ import com.example.repository.CredencialRepository;
 import com.example.repository.RolRepository;
 import com.example.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
 import java.util.Date;
 
 @Service
 public class RegistroUsuarioService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RegistroUsuarioService.class);
 
     @Autowired
     private PersonaRepository personaRepository;
@@ -31,21 +36,30 @@ public class RegistroUsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public Usuario registrarUsuario(RegistroUsuarioDTO registroUsuarioDTO) {
+        logger.info("Iniciando el registro del usuario");
+        
         // Usar el mapper para convertir DTO a entidades
         Persona persona = RegistroUsuarioMapper.INSTANCE.registroUsuarioDTOToPersona(registroUsuarioDTO);
         Credencial credencial = RegistroUsuarioMapper.INSTANCE.registroUsuarioDTOToCredencial(registroUsuarioDTO);
         Usuario usuario = RegistroUsuarioMapper.INSTANCE.registroUsuarioDTOToUsuario(registroUsuarioDTO);
 
         // Guardar Persona
+        logger.info("Guardando la persona");
         persona = personaRepository.save(persona);
 
-        // Guardar Credencial
+        // Cifrar la contraseña antes de guardar Credencial
+        logger.info("Cifrando la contraseña y guardando la credencial");
         credencial.setEmail(persona.getEmail()); // Usar el mismo email de Persona
+        credencial.setContraseña(passwordEncoder.encode(credencial.getContraseña()));
         credencial = credencialRepository.save(credencial);
 
         // Configurar y guardar Usuario
+        logger.info("Configurando y guardando el usuario");
         usuario.setPersona(persona);
         usuario.setRol(rolRepository.findById(registroUsuarioDTO.getRolId()).orElseThrow(() -> new RuntimeException("Rol no encontrado")));
         usuario.setCredencial(credencial);
@@ -53,7 +67,9 @@ public class RegistroUsuarioService {
         // Establecer la fecha de registro actual
         usuario.setFecha_registro(new Timestamp(new Date().getTime()));
 
-        return usuarioRepository.save(usuario);
+        Usuario savedUsuario = usuarioRepository.save(usuario);
+        logger.info("Usuario registrado exitosamente");
+
+        return savedUsuario;
     }
 }
-
